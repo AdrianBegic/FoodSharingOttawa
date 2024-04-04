a9adfd8e67c96e36a9866f91864db3749fd4caed
Adrian Begic-Jovanovic <88903998+AdrianBegic@users.noreply.github.com>
2024-04-04 01:23:51 -0400

    Delete Prototype3 directory

--- Prototype3/scripts/Volunteerscript.js
+++
@@ -1,9 +0,0 @@
-function openDonationPopup() {
-    document.getElementById("donationPopup").style.display = "block";
-    document.getElementById("overlay").style.display = "block";
-}
-
-function closeDonationPopup() {
-    document.getElementById("donationPopup").style.display = "none";
-    document.getElementById("overlay").style.display = "none";
-}
